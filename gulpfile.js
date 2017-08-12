d722eebe61dc4f07d8b4e0b06575fd509a03f4c5
Mike Ngu <mikengu01@gmail.com>
2017-08-11 19:27:51 -0500

    add gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const plugins = require('gulp-load-plugins')();
+
 
 gulp.task('styles', () => {
     return gulp.src('sass/styles.scss')
-        .pipe(sass())
+        .pipe(plugins.sass().on('error', plugins.sass.logError))
         .pipe(gulp.dest('./css'));
 });
 
