8272e1ff736f8b25a55800fe933c8eda9a21ac6b
Mike Ngu <mikengu01@gmail.com>
2017-08-27 12:18:50 -0500

    Add boilerplate code

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
-const gulp = require('gulp');
-const plugins = require('gulp-load-plugins')();
+const gulp          = require('gulp');
+const browserSync   = require('browser-sync').create();
+const sass          = require('gulp-sass');
 
-
-gulp.task('styles', () => {
-    return gulp.src('./sass/styles.scss')
-        .pipe(plugins.sass()
-        .on('error', plugins.sass.logError))
-        .pipe(plugins.cssmin())
-        .pipe(plugins.autoprefixer())
-        .pipe(gulp.dest('./css'));
+gulp.task('sass', function() {
+    return gulp.src(['src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('watch', () => {
-    gulp.watch('./sass/**/*.scss', ['styles']);
+
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: './src'
+    });
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    gulp.watch(['src/*.html']).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['styles']);
+gulp.task('default', ['serve']);
